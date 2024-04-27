842cbfd70598eab50bf36b24586f5bc248fb3db1
wsligor <wsligor@gmail.com>
2024-04-27 18:26:36 +0300

    add models.py add admin.py

--- search/views.py
+++ search/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Sku
 
 
 # Create your views here.
 def searchListView(request):
-    return render(request, 'search/searchList.html')
+    sku = Sku.objects.all()
+    return render(request, 'search/searchList.html', {'sku': sku})
